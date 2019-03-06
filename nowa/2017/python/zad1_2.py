5cb781f34920aa83c2300962403d942a947c2c2a
shilangyu <xmarcinmarcin@gmail.com>
2019-03-06 16:01:22 +0100

    changed function name to english

--- nowa/2017/python/zad1_2.py
+++ nowa/2017/python/zad1_2.py
@@ -2,13 +2,13 @@ with open('../dane/dane.txt') as f:
     data = [x[:-1].split(' ') for x in f]
 
 
-def jest_palindromem(line):
+def is_palindrome(line):
     return line == [*reversed(line)]
 
 
 not_pali = 0
 for ln in data:
-    if not jest_palindromem(ln):
+    if not is_palindrome(ln):
         not_pali += 1
 
 answer = f'6.2) Trzeba usunac {not_pali} wierszy'
