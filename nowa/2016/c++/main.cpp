0f8849cbbaf26234b64c2f81d4c931d8ca09f430
Kosodrzewinatru <sienkiew@live.com>
2019-03-09 21:40:23 +0100

    updated main file

--- nowa/2016/c++/main.cpp
+++ nowa/2016/c++/main.cpp
@@ -1,8 +1,11 @@
 #include "zad6_1.cpp"
+#include "zad6_2.cpp"
 #include <fstream>
 using namespace std;
 
 int main()
 {
-    cout << zad6_1();
+    cout << zad6_1() << endl
+         << endl
+         << zad6_2();
 }
