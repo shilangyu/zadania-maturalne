4b625732165093d5727396a6c32277f9927e746d
Kosodrzewinatru <sienkiew@live.com>
2019-03-10 13:27:13 +0100

    added splitting of content vector

--- nowa/2016/c++/zad6_3.cpp
+++ nowa/2016/c++/zad6_3.cpp
@@ -18,5 +18,40 @@ string zad6_3()
             content.push_back(line);
     }
 
+    //rozdzielanie vectora z całą zawartością na dwa oddzielne vectory z początkowymi wyrazami i zaszyfrowanymi
+    vector<string> rawData;
+    vector<string> encodedData;
+    bool switchVector = false;
+    string readyToPush = "";
+
+    for (int i = 0; i < content.size(); i++)
+    {
+        for (int j = 0; j < content[i].size(); j++)
+        {
+            if (switchVector)
+            {
+                readyToPush += content[i][j];
+
+                if (j == content[i].size() - 1)
+                {
+                    encodedData.push_back(readyToPush);
+                    readyToPush = "";
+                    switchVector = false;
+                }
+            }
+            else
+            {
+                if (content[i][j] != ' ')
+                    readyToPush += content[i][j];
+                else
+                {
+                    rawData.push_back(readyToPush);
+                    switchVector = true;
+                    readyToPush = "";
+                }
+            }
+        }
+    }
+
     return "";
 }
