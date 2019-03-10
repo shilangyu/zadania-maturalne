f60b159d946c5dec82f5b9e1992e81ba8c693e92
Kosodrzewinatru <sienkiew@live.com>
2019-03-10 15:26:54 +0100

    splitting function was actually good

--- nowa/2016/c++/zad6_3.cpp
+++ nowa/2016/c++/zad6_3.cpp
@@ -5,6 +5,50 @@
 #include <vector>
 using namespace std;
 
+//funkcja rozdzielająca vector z całą zawartością na dwa oddzielne vectory z początkowymi wyrazami i zaszyfrowanymi
+vector<vector<string>> splitVector(vector<string> input)
+{
+    vector<string> rawData;
+    vector<string> encodedData;
+    vector<vector<string>> output;
+    bool switchVector = false;
+    string readyToPush = "";
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        for (int j = 0; j < input[i].size(); j++)
+        {
+            if (switchVector)
+            {
+                readyToPush += input[i][j];
+
+                if (j == input[i].size() - 1)
+                {
+                    encodedData.push_back(readyToPush);
+                    readyToPush = "";
+                    switchVector = false;
+                }
+            }
+            else
+            {
+                if (input[i][j] != ' ')
+                    readyToPush += input[i][j];
+                else
+                {
+                    rawData.push_back(readyToPush);
+                    switchVector = true;
+                    readyToPush = "";
+                }
+            }
+        }
+    }
+
+    output.push_back(rawData);
+    output.push_back(encodedData);
+
+    return output;
+}
+
 string zad6_3()
 {
     string line;
@@ -18,6 +62,10 @@ string zad6_3()
         while (getline(file, line))
             content.push_back(line);
     }
+
+    //wywoływanie funkcji splitVector
+    vector<string> raw = splitVector(content)[0];
+    vector<string> encoded = splitVector(content)[1];
 
     return "";
 }
