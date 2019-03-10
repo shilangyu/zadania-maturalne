e726854abbfb3c1c20ef8850cea0856a592eeb03
Kosodrzewinatru <sienkiew@live.com>
2019-03-10 12:59:09 +0100

    added zad6_3 file

---
+++ nowa/2016/c++/zad6_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+string zad6_3()
+{
+    string line;
+    vector<string> content;
+
+    fstream file("../dane/dane_6_3.txt");
+
+    if (file.is_open())
+    {
+        while (getline(file, line))
+            content.push_back(line);
+    }
+
+    return "";
+}
