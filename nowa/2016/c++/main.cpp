d00ee73a7e9db83f850c71b29cbe0d3bcedb151a
Kosodrzewinatru <sienkiew@live.com>
2019-03-11 16:36:38 +0100

    main generate now a txt file with all answers

--- nowa/2016/c++/main.cpp
+++ nowa/2016/c++/main.cpp
@@ -1,5 +1,6 @@
 #include "zad6_1.cpp"
 #include "zad6_2.cpp"
+#include "zad6_3.cpp"
 #include <fstream>
 using namespace std;
 
@@ -7,5 +8,15 @@ int main()
 {
     cout << zad6_1() << endl
          << endl
-         << zad6_2();
+         << zad6_2() << endl
+         << endl
+         << zad6_3();
+
+    ofstream result("wyniki.txt");
+
+    result << zad6_1() << endl
+           << endl
+           << zad6_2() << endl
+           << endl
+           << zad6_3();
 }
