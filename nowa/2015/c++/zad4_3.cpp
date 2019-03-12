b2ed98d719f374e78f717b8e2b3770c210d14ad2
Kosodrzewinatru <sienkiew@live.com>
2019-03-12 12:43:16 +0100

    added first sort extraction from content

--- nowa/2015/c++/zad4_3.cpp
+++ nowa/2015/c++/zad4_3.cpp
@@ -20,5 +20,43 @@ string zad4_3()
     }
     file.close();
 
+    int maxInRow = 0, currentMaxInRow = 0;
+
+    //szukanie maksymalnego ciągu jedynek znajdującego się z przodu liczby
+    for (int i = 0; i < content.size(); i++)
+    {
+        for (int j = 0; j < content[i].size(); j++)
+        {
+            if (content[i][j] == '1')
+                currentMaxInRow++;
+            else
+                break;
+        }
+
+        if (currentMaxInRow > maxInRow)
+            maxInRow = currentMaxInRow;
+
+        currentMaxInRow = 0;
+    }
+
+    //wyodrębnianie tych liczb, które mają maksymalnej długości ciąg składający się z jedynek z przodu liczby
+    vector<string> firstSort;
+
+    for (int i = 0; i < content.size(); i++)
+    {
+        for (int j = 0; j < content[i].size(); j++)
+        {
+            if (content[i][j] == '1')
+                currentMaxInRow++;
+            else
+                break;
+        }
+
+        if (currentMaxInRow == maxInRow)
+            firstSort.push_back(content[i]);
+
+        currentMaxInRow = 0;
+    }
+
     return "";
 }
