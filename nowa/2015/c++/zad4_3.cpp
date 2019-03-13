3b9615daa15b6e131a24e4e91e0f0a25ce8412ed
Kosodrzewinatru <sienkiew@live.com>
2019-03-13 19:46:38 +0100

    started all from the beginning

--- nowa/2015/c++/zad4_3.cpp
+++ nowa/2015/c++/zad4_3.cpp
@@ -20,26 +20,6 @@ string zad4_3()
     }
     file.close();
 
-    int maxInRow = 0, currentMaxInRow = 0;
-
-    //szukanie maksymalnego ciągu jedynek znajdującego się z przodu liczby
-    for (int i = 0; i < content.size(); i++)
-    {
-        for (int j = 0; j < content[i].size(); j++)
-        {
-            if (content[i][j] == '1')
-                currentMaxInRow++;
-            else
-                break;
-        }
-
-        if (currentMaxInRow > maxInRow)
-            maxInRow = currentMaxInRow;
-
-        currentMaxInRow = 0;
-    }
-
-    //znajdowanie liczby odpowiadającej za rozmiar najdłuższej liczby/liczb
     int maxLength = 0;
 
     for (int i = 0; i < content.size(); i++)
@@ -48,37 +28,5 @@ string zad4_3()
             maxLength = content[i].size();
     }
 
-    //wyodrębnianie najdłuższych liczb
-    vector<string> firstSort;
-
-    for (int i = 0; i < content.size(); i++)
-    {
-        if (content[i].size() == maxLength)
-            firstSort.push_back(content[i]);
-    }
-
-    //wyodrębnianie tych liczb, które mają maksymalnej długości ciąg składający się z jedynek z przodu liczby
-    vector<string> secondSort;
-
-    for (int i = 0; i < firstSort.size(); i++)
-    {
-        for (int j = 0; j < firstSort[i].size(); j++)
-        {
-            if (firstSort[i][j] == '1')
-                currentMaxInRow++;
-            else
-                break;
-        }
-
-        if (currentMaxInRow == maxInRow)
-            secondSort.push_back(firstSort[i]);
-
-        currentMaxInRow = 0;
-    }
-
-    for (int i = 0; i < firstSort.size(); i++)
-    {
-    }
-
-    return "";
+    return to_string(maxLength);
 }
