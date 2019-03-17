3ffc667f7bd61d867fea502d682140b4404674ed
Kosodrzewinatru <sienkiew@live.com>
2019-03-17 16:52:00 +0100

    modified main file, now it includes all of subtasks

---
+++ nowa/2018/c++/main.cpp
@@ -0,0 +1,11 @@
+#include "zad4_1.cpp"
+#include "zad4_2.cpp"
+#include "zad4_3.cpp"
+#include <fstream>
+
+int main()
+{
+    cout << zad4_1() << endl
+         << zad4_2() << endl
+         << zad4_3();
+}
