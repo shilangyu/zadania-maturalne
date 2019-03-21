8c0790ece87241158967e084b5a38d4c513862b3
Kosodrzewinatru <sienkiew@live.com>
2019-03-21 14:56:42 +0100

    commented zad4_2

--- stara/2015/c++/zad4_2.cpp
+++ stara/2015/c++/zad4_2.cpp
@@ -22,20 +22,27 @@ string zad4_2()
 
     int howMany = 0;
 
+    //przechodzenie po vectorze z zawartością
     for (int i = 0; i < content.size(); i++)
     {
+        //przechodzimy dalej tylko jeśli pierwszym elementen stringa jest 0
         if (content[i][0] == '0')
         {
             bool switchedChar = false;
 
+            //przechodzenie po pojedynczym stringu
             for (int j = 0; j < content[i].size(); j++)
             {
+                //jeżeli po raz pierwszy zmienia się znak, zmieniamy wartość switchedChar na true
                 if (content[i][j] == '1' and switchedChar == false)
                     switchedChar = true;
 
+                //jeżeli znak zmienił się po raz kolejny wychodzimy z pętli
                 if (content[i][j] == '0' and switchedChar)
                     break;
 
+                //jeżeli jesteśmy na ostatnim przejściu oraz sprawdzimy, że wszystkie wymogi zostały spełnione,
+                //dodajemy jeden do intigera howMany
                 if (j == content[i].size() - 1 and switchedChar and content[i][j] == '1')
                     howMany++;
             }
