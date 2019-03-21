23074a239262e22c44897e452361f204f2b44b2b
Kosodrzewinatru <sienkiew@live.com>
2019-03-21 13:13:31 +0100

    main file displays the results

---
+++ stara/2017/c++/main.cpp
@@ -0,0 +1,14 @@
+#include "zad4_1.cpp"
+#include "zad4_2.cpp"
+#include "zad4_3.cpp"
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    cout << zad4_1() << endl
+         << endl
+         << zad4_2() << endl
+         << endl
+         << zad4_3();
+}
