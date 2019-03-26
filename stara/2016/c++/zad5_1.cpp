ead4e8a5e2c64c121be9aab75fd7314c30ec464c
Kosodrzewinatru <sienkiew@live.com>
2019-03-26 17:49:53 +0100

    got rows and cols of the board

--- stara/2016/c++/zad5_1.cpp
+++ stara/2016/c++/zad5_1.cpp
@@ -8,10 +8,11 @@ using namespace std;
 string zad5_1()
 {
     string line;
-    char board[12][20];
+    char boardOriginal[12][20];
 
     fstream file("../dane/gra.txt");
 
+    //wczytywanie danych z pliku do dwuwymiarowej tablicy
     if (file.is_open())
     {
         int j = 0;
@@ -19,17 +20,17 @@ string zad5_1()
         while (getline(file, line))
         {
             for (int i = 0; i < line.size(); i++)
-                board[j][i] = line[i];
+                boardOriginal[j][i] = line[i];
 
             j++;
         }
     }
     file.close();
 
-    int rows = sizeof board / sizeof board[0]; // ilość wierszy
-    int cols = sizeof board / rows;            // ilość kolumn
+    int rows = sizeof boardOriginal / sizeof originalBoard[0]; // ilość wierszy w oryginalnej tablicy
+    int cols = sizeof boardOriginal / rows;                    // ilość kolumn w oryginalnej tablicy
 
-    ofstream test("test.txt");
+    char boardFake[13][21];
 
     return "";
 }
