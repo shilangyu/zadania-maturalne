01d0bd723234d415e6dfb6a322232da0b65da696
Kosodrzewinatru <sienkiew@live.com>
2019-03-27 16:42:54 +0100

    added extended function

--- stara/2016/c++/zad5_1.cpp
+++ stara/2016/c++/zad5_1.cpp
@@ -5,99 +5,42 @@
 #include <vector>
 using namespace std;
 
-vector<vector<string>> extend(char boardOriginal)
+vector<string> extend(vector<string> original)
 {
-    char boardExtended[13][21]; // tablica rozszerzona o jeden wiersz i kolumnę, by pozbyć się problemu z zawijaniem
+    vector<string> extended = original;
 
-    int rowsExtended = sizeof boardExtended / sizeof boardExtended[0]; // ilość wierszy w rozszerzonej tablicy
-    int colsExtended = sizeof boardExtended / rowsExtended;            // ilość kolumn w rozszerzonej tablicy
+    for (int i = 0; i < extended.size(); i++)
+        extended[i] += extended[i][0];
 
-    for (int i = 0; i < rowsExtended; i++)
-    {
-        for (int j = 0; j < colsExtended; j++)
-        {
-            if (i != rowsExtended - 1)
-            {
-                if (j == colsExtended - 1)
-                    boardExtended[i][j] = boardExtended[i][0];
-                else
-                    boardExtended[i][j] = boardOriginal[i][j];
-            }
-            else
-            {
-                if (j == colsExtended - 1)
-                    boardExtended[i][j] = boardExtended[i][0];
-                else
-                    boardExtended[i][j] = boardOriginal[0][j];
-            }
-        }
-    }
+    extended.push_back(extended[0]);
 
-    return boardExtended;
+    extended[extended.size() - 1][extended[extended.size() - 1].size() - 1] = extended[0][0];
+
+    return extended;
 }
 
 string zad5_1()
 {
     string line;
-    char boardOriginal[12][20];
+
+    vector<string> boardOriginal;
 
     fstream file("../dane/gra.txt");
 
-    //wczytywanie danych z pliku do dwuwymiarowej tablicy
+    //wczytywanie danych z pliku
     if (file.is_open())
     {
-        int j = 0;
-
         while (getline(file, line))
-        {
-            for (int i = 0; i < line.size(); i++)
-                boardOriginal[j][i] = line[i];
-
-            j++;
-        }
+            boardOriginal.push_back(line);
     }
     file.close();
 
-    int rowsOriginal = sizeof boardOriginal / sizeof boardOriginal[0]; // ilość wierszy w oryginalnej tablicy
-    int colsOriginal = sizeof boardOriginal / rowsOriginal;            // ilość kolumn w oryginalnej tablicy
-
-    // char boardExtended[13][21]; // tablica rozszerzona o jeden wiersz i kolumnę, by pozbyć się problemu z zawijaniem
-
-    // int rowsExtended = sizeof boardExtended / sizeof boardExtended[0]; // ilość wierszy w rozszerzonej tablicy
-    // int colsExtended = sizeof boardExtended / rowsExtended;            // ilość kolumn w rozszerzonej tablicy
-
-    // for (int i = 0; i < rowsExtended; i++)
-    // {
-    //     for (int j = 0; j < colsExtended; j++)
-    //     {
-    //         if (i != rowsExtended - 1)
-    //         {
-    //             if (j == colsExtended - 1)
-    //                 boardExtended[i][j] = boardExtended[i][0];
-    //             else
-    //                 boardExtended[i][j] = boardOriginal[i][j];
-    //         }
-    //         else
-    //         {
-    //             if (j == colsExtended - 1)
-    //                 boardExtended[i][j] = boardExtended[i][0];
-    //             else
-    //                 boardExtended[i][j] = boardOriginal[0][j];
-    //         }
-    //     }
-    // }
-
-    char boardExtended[13][21] = extend(boardOriginal);
+    vector<string> boardExtended = extend(boardOriginal);
 
     ofstream test("test.txt");
 
-    for (int i = 0; i < rowsExtended; i++)
-    {
-        for (int j = 0; j < colsExtended; j++)
-            test << boardExtended[i][j];
-
-        test << endl;
-    }
+    for (int i = 0; i < boardExtended.size(); i++)
+        test << boardExtended[i] << endl;
 
     return "";
 }
