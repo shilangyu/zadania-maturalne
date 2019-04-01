1a43ce6a9b368f0174373332033dd9d2709a9922
Kosodrzewinatru <sienkiew@live.com>
2019-04-01 23:41:31 +0200

    added proper main file

--- stara/2016/c++/main.cpp
+++ stara/2016/c++/main.cpp
@@ -6,5 +6,17 @@ using namespace std;
 
 int main()
 {
-    cout << zad5_1();
+    cout << zad5_1() << endl
+         << endl
+         << zad5_2() << endl
+         << endl
+         << zad5_3();
+
+    ofstream result("wyniki.txt");
+
+    result << zad5_1() << endl
+           << endl
+           << zad5_2() << endl
+           << endl
+           << zad5_3();
 }
