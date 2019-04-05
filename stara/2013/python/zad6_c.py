101c820a282f8203cf823fd798c6e5f2e1916a37
shilangyu <xmarcinmarcin@gmail.com>
2019-04-05 18:42:44 +0200

    stara/2013 done

---
+++ stara/2013/python/zad6_c.py
@@ -0,0 +1,36 @@
+# wczytanie napisow
+with open('../dane/dane.txt') as f:
+    data = []
+    for word in f.readlines():
+        data.append(word[:-1])
+
+
+# licznik
+not_shrinking = 0
+# rekordy
+biggest_none_shrinking = '0'
+smallest_none_shrinking = '7'*20
+# przejscie po slowach
+for word in data:
+    # sprawdzenie czy niemaleje
+    is_not_shrinking = True
+    prev_num = 0
+    for char in word:
+        if int(char) < prev_num:
+            is_not_shrinking = False
+            break
+        prev_num = int(char)
+
+    if is_not_shrinking:
+        not_shrinking += 1
+        # jezeli pobilo rekord wielkosci zapisz
+        if int(biggest_none_shrinking, base=8) < int(word, base=8):
+            biggest_none_shrinking = word
+        # jezeli pobilo rekord mniejszosci zapisz
+        if int(smallest_none_shrinking, base=8) > int(word, base=8):
+            smallest_none_shrinking = word
+
+
+# wyswietlenie odpowiedzi
+answer = f'6 c) Ilosc niemalejacych liczb: {not_shrinking}; Najmniejsza taka liczba: {smallest_none_shrinking}(8) {int(smallest_none_shrinking, base=8)}(10); Najwieksza taka liczba: {biggest_none_shrinking}(8) {int(biggest_none_shrinking, base=8)}(10)'
+print(answer)
