c950c4a817e409847f2e80bc4629c30387e501eb
Kosodrzewinatru <sienkiew@live.com>
2019-04-06 14:42:07 +0200

    added reading from file and isAnagram function

---
+++ stara/2010/c++/zad4_b.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// funkcja sprawdzajaca czy dwa slowa sa anagramami
+bool isAnagram(string first, string second)
+{
+    vector<char> secondWord;
+
+    if (first.size() == second.size())
+    {
+
+        for (int i = 0; i < first.size(); i++)
+            secondWord.push_back(second[i]);
+
+        bool checker;
+
+        for (int i = 0; i < first.size(); i++)
+        {
+            for (int j = 0; j < secondWord.size(); j++)
+            {
+                if (first[i] == secondWord[j])
+                {
+                    secondWord[j] = '-';
+                    checker = true;
+                    break;
+                }
+                else if (first[i] != secondWord[j])
+                    checker = false;
+            }
+
+            if (checker == false)
+                return false;
+        }
+    }
+    else
+        return false;
+
+    for (int i = 0; i < secondWord.size(); i++)
+    {
+        if (secondWord[i] != '-')
+            return false;
+    }
+
+    return true;
+}
+
+string zad4_a()
+{
+    string line;
+    vector<string> content;
+
+    // wczytywanie danych z pliku
+    fstream file("../dane/anagram.txt");
+
+    if (file.is_open())
+    {
+        while (getline(file, line))
+            content.push_back(line);
+    }
+    file.close();
+}
