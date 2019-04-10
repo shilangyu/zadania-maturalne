ef0b8be6fa51f409108771b4bd60e6bd7912d964
Kosodrzewinatru <sienkiew@live.com>
2019-04-10 16:08:02 +0200

    added splitting vector

--- stara/2009/c++/zad5_a.cpp
+++ stara/2009/c++/zad5_a.cpp
@@ -20,5 +20,38 @@ string zad5_a()
     }
     file.close();
 
+    vector<string> wordsA;
+    vector<string> wordsB;
+
+    // rozdzielanie zawartości vectora na dwa oddzielne vectory
+    for (int i = 0; i < content.size(); i++)
+    {
+        string readyToPush = "";
+        bool afterGap = false, wasPushed = false;
+
+        for (int j = 0; j < content[i].size(); j++)
+        {
+            if (content[i][j] == ' ')
+            {
+                afterGap = true;
+            }
+            else if (afterGap)
+            {
+                if (wasPushed == false)
+                {
+                    wordsA.push_back(readyToPush);
+                    wasPushed = true;
+                    readyToPush = "";
+                }
+
+                readyToPush += content[i][j];
+            }
+            else
+                readyToPush += content[i][j];
+        }
+
+        wordsB.push_back(readyToPush);
+    }
+
     return "";
 }
