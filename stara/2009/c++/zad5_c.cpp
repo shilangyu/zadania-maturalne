44f7988e0d18dfeac6d62b97d52e8afd6fdb2ec1
Kosodrzewinatru <sienkiew@live.com>
2019-04-11 10:29:28 +0200

    started zad5_c

---
+++ stara/2009/c++/zad5_c.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+//funkcja sprawdzająca czy jeden string zawiera się w drugim
+bool isIn1(string contener, string element)
+{
+    for (int i = 0; i < contener.size(); i++)
+    {
+        if (contener[i] == element[0])
+        {
+            for (int j = 0; j < element.size(); j++)
+            {
+                if (element[j] != contener[i + j])
+                    break;
+
+                if (j == element.size() - 1)
+                    return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+string zad5_c()
+{
+    string line;
+    vector<string> content;
+
+    // wczytywanie danych z pliku
+    fstream file("../dane/dane.txt");
+
+    if (file.is_open())
+    {
+        while (getline(file, line))
+            content.push_back(line);
+    }
+    file.close();
+
+    vector<string> wordsA;
+    vector<string> wordsB;
+
+    // rozdzielanie zawartości vectora na dwa oddzielne vectory
+    for (int i = 0; i < content.size(); i++)
+    {
+        string readyToPush = "";
+        bool afterGap = false, wasPushed = false;
+
+        for (int j = 0; j < content[i].size(); j++)
+        {
+            if (content[i][j] == ' ')
+            {
+                afterGap = true;
+            }
+            else if (afterGap)
+            {
+                if (wasPushed == false)
+                {
+                    wordsA.push_back(readyToPush);
+                    wasPushed = true;
+                    readyToPush = "";
+                }
+
+                readyToPush += content[i][j];
+            }
+            else
+            {
+                readyToPush += content[i][j];
+            }
+        }
+
+        wordsB.push_back(readyToPush);
+    }
+
+    return "";
+}
