02c7875b3a3456202108e1ede1b386f89053877d
Kosodrzewinatru <sienkiew@live.com>
2019-05-09 08:24:07 +0200

    added function isMoreThanOne and answered to the first part of zad6_a

--- stara/2006/c++/zad6_a.cpp
+++ stara/2006/c++/zad6_a.cpp
@@ -1,1 +1,57 @@
- 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+// funkcja sprawdzajaca czy w danym vectorze dane slowo wystepuje wiecej niz jeden raz
+bool isMoreThanOne(string word, vector<string> vec)
+{
+    int counter = 0;
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (word == vec[i])
+        {
+            counter++;
+        }
+    }
+
+    if (counter > 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+string zad6_a()
+{
+    string line;
+    fstream file("../dane/dane.txt");
+    vector<string> content;
+
+    // wczytywanie elementow z pliku
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            content.push_back(line);
+        }
+    }
+    file.close();
+
+    int howManyMoreThanOne = 0;
+
+    for (int i = 0; i < content.size(); i++)
+    {
+        if (isMoreThanOne(content[i], content))
+        {
+            howManyMoreThanOne++;
+        }
+    }
+
+    string answer = "6 a) Liczba slow wystepujacych w pliku wiecej niz jeden raz: " + to_string(howManyMoreThanOne);
+}
