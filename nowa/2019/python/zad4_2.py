7049e1d588ed0c035a9c8495d4b492fc3ac2ae28
krawieck <fil.krawczyk@gmail.com>
2019-08-22 22:39:24 +0200

    added excercise 2

---
+++ nowa/2019/python/zad4_2.py
@@ -0,0 +1,17 @@
+from math import factorial
+
+
+def factorial_sum(num) -> int:
+    return sum(map(lambda x: factorial(int(x)), list(num)))
+
+
+data = [x[:-1] for x in open('../dane/liczby.txt')]
+
+numbers = []
+
+for x in data:
+    if int(x) == factorial_sum(x):
+        numbers.append(x)
+
+final_nums = '\n'.join(numbers)
+answer = f'4.2. Liczby rowne silni swoich cyfr: \n{final_nums}'
