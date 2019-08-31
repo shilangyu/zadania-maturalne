d87c86b9116e2b8b611c5c4c891879228a1430ae
Kosodrzewinatru <sienkiew@live.com>
2019-08-31 18:50:33 +0200

    wrote main

---
+++ nowa/2019/c++/main.cpp
@@ -0,0 +1,20 @@
+#include "zad4_1.cpp"
+#include "zad4_2.cpp"
+#include "zad4_3.cpp"
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    cout << zad4_1() << endl
+         << endl
+         << zad4_2() << endl
+         << zad4_3();
+
+    ofstream result("wyniki.txt");
+
+    result << zad4_1() << endl
+           << endl
+           << zad4_2() << endl
+           << zad4_3();
+}
