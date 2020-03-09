1fca854449bdd6ec80eff64155fccb7d706c1cc5
krawieck <fil.krawczyk@gmail.com>
2020-03-09 21:28:15 +0100

    add documentation

--- nowa/2019/python/zad4_1.py
+++ nowa/2019/python/zad4_1.py
@@ -1,12 +1,16 @@
+# zaladuj dane
 data = [int(x[:-1]) for x in open('../dane/liczby.txt')]
 
 
 def is_power_of(x, n):
-    """checks if `x` is the power of `n`"""
+    """sprawdza czy x jest potega liczby n"""
+
+    # najpierw wyeliminowac 2 przypadki gdzie wiadomo ze liczba napewno spelnia lub nie spelnia warukow
     if x == 1:
         return True
     if x % n != 0:
         return False
+    # dzielic liczbe x az bedzie sie rownala (badz nie) n
     while x >= n:
         if x == n:
             return True
@@ -16,9 +20,10 @@ def is_power_of(x, n):
 
 count = 0
 
-
+# przejdz po danych i policz potegi liczby 3
 for x in data:
     count += is_power_of(x, 3)
 
+# zapisz odpowiedz
 
 answer = f'4.1. Ilosc liczb ktore sa potegami liczby 3: {count}'
--- nowa/2019/python/zad4_2.py
+++ nowa/2019/python/zad4_2.py
@@ -1,21 +1,27 @@
 from math import factorial
 
-
+# zaladuj dane
 data = [x[:-1] for x in open('../dane/liczby.txt')]
 
 
 def factorial_sum(num):
+    """zsumuj silnie kazdego ze znakow liczby"""
+
     processed = []
+    # podziel liczbe na liste znakow
     for x in list(num):
+        # dodaj do `processed` silnie kazdego ze znakow
         processed.append(factorial(int(x)))
     return sum(processed)
 
 
 numbers = []
 
+# przejdz po numerach i sprawdz ktore sumy silni sie rownaja silni samej liczby
 for x in data:
     if int(x) == factorial_sum(x):
         numbers.append(x)
 
+# zapisz odpowiedz
 final_nums = '\n'.join(numbers)
 answer = f'4.2. liczby rowne sumie silni swoich liczb: \n{final_nums}'
